73e28d3b318111fa846f42dd27ecd87f669386df
BlackSpy <48418515+blackspyek@users.noreply.github.com>
2024-10-10 22:32:54 +0200

    fix/ changed to world changed to world

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("Hello!");
 
 
     }
